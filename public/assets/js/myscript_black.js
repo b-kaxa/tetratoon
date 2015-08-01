22610db0931172ab9597e49834a48a0b78733960
Kazuki Bandai <nisigin@gmail.com>
2015-08-01 16:04:32 +0000

    modify

--- public/assets/js/myscript_black.js
+++ public/assets/js/myscript_black.js
@@ -14,45 +14,50 @@ $(function(){
       alertify.alert( winner + "!!!");
     }
   }
-  
+
   $("td").click(function(){
-    // clickしたブロックのid
-    var clicked_id = $(this).prop("id");
 
-    // next blockの色の付いているブロックのidを取得
-    var diff = [];
-    $($("#next_blocks td.black").get().reverse()).each(function(i, elem) {
-        diff[i] = $(elem).prop("id")
-    });
+    if($(this).hasClass('black')){
 
-    // 差分を出す
-    var small_block_diff = [];
-    for (var i = 0; i < diff.length; i++) {
-       small_block_diff[i] = diff[i] - diff[0];
-
-       if(-5 <= small_block_diff[i] && -3 >= small_block_diff[i]) {
-        small_block_diff[i] -= 7
-       } else if (-8 <= small_block_diff[i] && -6 >= small_block_diff[i]) {
-        small_block_diff[i] -= 14
-       }
-    }
-
-    var update_blocks = small_block_diff.map(function(diff) {
-                return parseInt(clicked_id) + diff;
-              });
-
-    // ajaxで/updateにpostする
-    $.ajax({
-      type: "POST",
-      url: "/update",
-      data: {
-        data : 2,
-        update_blocks: update_blocks
-      },
-      success: function(){
-        location.href="/";
+      // clickしたブロックのid
+      var clicked_id = $(this).prop("id");
+  
+      // next blockの色の付いているブロックのidを取得
+      var diff = [];
+      $($("#next_blocks td.black").get().reverse()).each(function(i, elem) {
+          diff[i] = $(elem).prop("id")
+      });
+  
+      // 差分を出す
+      var small_block_diff = [];
+      for (var i = 0; i < diff.length; i++) {
+         small_block_diff[i] = diff[i] - diff[0];
+  
+         if(-5 <= small_block_diff[i] && -3 >= small_block_diff[i]) {
+          small_block_diff[i] -= 7
+         } else if (-8 <= small_block_diff[i] && -6 >= small_block_diff[i]) {
+          small_block_diff[i] -= 14
+         }
       }
-    });
-    
+  
+      var update_blocks = small_block_diff.map(function(diff) {
+                  return parseInt(clicked_id) + diff;
+                });
+  
+      // ajaxで/updateにpostする
+      $.ajax({
+        type: "POST",
+        url: "/update",
+        data: {
+          data : 2,
+          update_blocks: update_blocks
+        },
+        success: function(){
+          location.href="/";
+        }
+      });
+    } else {
+
+    }
   });
 });
--- public/assets/js/myscript_white.js
+++ public/assets/js/myscript_white.js
@@ -16,43 +16,48 @@ $(function(){
   }
 
   $("td").click(function(){
-    // clickしたブロックのid
-    var clicked_id = $(this).prop("id");
-
-    // next blockの色の付いているブロックのidを取得
-    var diff = [];
-    $("#next_blocks td.white").each(function(i, elem) {
-        diff[i] = $(elem).prop("id")
-    });
     
-    // 差分を出す
-    var small_block_diff = [];
-    for (var i = 0; i < diff.length; i++) {
-       small_block_diff[i] = diff[i] - diff[0];
+    if($(this).hasClass('white')){
 
-       if(3 <= small_block_diff[i] && 5 >= small_block_diff[i]) {
-        small_block_diff[i] += 7
-       } else if (6 <= small_block_diff[i] && 8 >= small_block_diff[i]) {
-        small_block_diff[i] += 14
-       }
-    }
-
-    var update_blocks = small_block_diff.map(function(diff) {
-                return parseInt(clicked_id) + diff;
-              });
-
-    // ajaxで/updateにpostする
-    $.ajax({
-      type: "POST",
-      url: "/update",
-      data: {
-        data : 1,
-        update_blocks: update_blocks
-      },
-      success: function(){
-        location.href="/black";
+      // clickしたブロックのid
+      var clicked_id = $(this).prop("id");
+  
+      // next blockの色の付いているブロックのidを取得
+      var diff = [];
+      $("#next_blocks td.white").each(function(i, elem) {
+          diff[i] = $(elem).prop("id")
+      });
+      
+      // 差分を出す
+      var small_block_diff = [];
+      for (var i = 0; i < diff.length; i++) {
+         small_block_diff[i] = diff[i] - diff[0];
+  
+         if(3 <= small_block_diff[i] && 5 >= small_block_diff[i]) {
+          small_block_diff[i] += 7
+         } else if (6 <= small_block_diff[i] && 8 >= small_block_diff[i]) {
+          small_block_diff[i] += 14
+         }
       }
-    });
-    
+  
+      var update_blocks = small_block_diff.map(function(diff) {
+                  return parseInt(clicked_id) + diff;
+                });
+  
+      // ajaxで/updateにpostする
+      $.ajax({
+        type: "POST",
+        url: "/update",
+        data: {
+          data : 1,
+          update_blocks: update_blocks
+        },
+        success: function(){
+          location.href="/black";
+        }
+      });
+    } else {
+      
+    }
   });
 });
